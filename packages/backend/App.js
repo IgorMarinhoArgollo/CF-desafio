8af1938621d36aff3311b0ded14e76c145bc74c8
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2023-01-05 18:38:48 -0300

    ci(Backend): :wrench: install rescue and creation of some files
    installation of express-rescue and creation of .env file a limiter config for Rate-Limit, creation of swagger file, creation of index.js and preload of app.js

---
+++ packages/backend/App.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const helmet = require('helmet');
+// const rescue = require('express-rescue');
+const swaggerUi = require('swagger-ui-express');
+const limiter = require('./limiter');
+require('dotenv').config();
+
+const swaggerDocs = require('./swagger.json');
+
+const app = express();
+app.use(helmet());
+app.use(limiter);
+app.use(express.json());
+
+// app.use('/login', rescue(loginRouter));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// app.use(errorMiddleware);
+
+module.exports = app;
