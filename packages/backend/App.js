f8bbe291c80a584da1651abdc9c531afb9699f27
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2023-01-12 14:23:57 -0300

    ci(Backend): :heavy_plus_sign: installing dependencies for testing
    installing mocha, chai, sinon for testing, nyc to evaluate coverage and preparing script for testing

--- packages/backend/App.js
+++ packages/backend/App.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const rescue = require('express-rescue');
 const swaggerUi = require('swagger-ui-express');
+const morgan = require('morgan');
 const limiter = require('./limiter');
 require('dotenv').config();
 const errorMiddleware = require('./middlewares/error');
@@ -13,10 +14,11 @@ const app = express();
 app.use(helmet());
 app.use(limiter);
 app.use(express.json());
+app.use(morgan('common'));
 
 app.use('/orders', rescue(orders));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(errorMiddleware);
 
