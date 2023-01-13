0b2fa73de76d20df78f5e2ae43266cdae721d93e
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2023-01-13 14:46:14 -0300

    feat(Frontend): :sparkles: base of the order in the table

---
+++ packages/frontend/src/components/OrderItem.vue
@@ -0,0 +1,82 @@
+<template>
+  <tr class="order-item">
+    <td>{{ nota }}</td>
+    <td>{{ sacado }}</td>
+    <td>{{ cedente }}</td>
+    <td>{{ emissao }}</td>
+    <td>{{ valor }}</td>
+    <td>{{ status }}</td>
+    <td>
+      <router-link
+        v-slot="{ navigate }"
+        to="/notFound"
+        custom
+      >
+        <button
+          role="link"
+          @click="navigate"
+        >
+          Dados do cedente
+        </button>
+      </router-link>
+    </td>
+  </tr>  
+</template>
+
+<script>
+export default {
+ props: {
+    nota: {
+      type: String,
+      default: '1234'},
+    sacado: {
+      type: String,
+      default: 'SACADO 001'},
+    cedente: {
+      type: String,
+      default: 'CEDENTE 002'},
+    emissao: {
+      type: String,
+      default: '12/02/2020'},
+    valor: {
+      type: String,
+      default: '49725,00'},
+    status: {
+      type: Number,
+      default: 1}
+  }
+}
+</script>
+
+<style>
+ td {
+  text-align: center;
+  padding: 15px 0;
+ }
+
+ button {
+  background-color: transparent;
+  width: 200px;
+  height: 35px;
+  border-radius: 25px;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  font-size: 0.8em;
+  font-weight: 600;
+  color: #727D94;
+  border-color: #DFE2EB;
+  border-style: solid;
+ }
+
+ @media screen and (max-width: 450px){
+tr {
+  font-size: 0.6em;
+}
+
+button {
+  width: 130px;
+  height: 25px;
+  font-size: 0.6em;
+ }
+
+}
+</style>
--- packages/frontend/src/views/MainContent.vue
+++ packages/frontend/src/views/MainContent.vue
@@ -6,21 +6,41 @@
           src="../assets/Vector-darkblue.svg"
           alt="hands logo"
         >
-        <h2>Notas Fiscais</h2>
+        <h2 class="title">Notas Fiscais</h2>
       </div>
       <p class="subtitle">
         Visualize as notas fiscais que você tem.
       </p>
     </div>
+    <table class="orders-table">
+      <thead>
+        <tr>
+          <th>NOTA FISCAL</th>
+          <th>SACADO</th>
+          <th>CEDENTE</th>
+          <th>EMISSÃO</th>
+          <th>VALOR</th>
+          <th>STATUS</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        <OrderItem nota="123" sacado="sacado1" cedente="cedente1" emissao="emissão" valor="123,23" status="1" />
+        <OrderItem nota="123" sacado="sacado1" cedente="cedente1" emissao="emissão" valor="123,23" status="1" />
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import OrderItem from '../components/OrderItem.vue'
 
 export default {
   name: "InformationApi",
-
+  components: {
+    OrderItem
+  },
 
    data() {
      return {
@@ -49,14 +69,12 @@ export default {
 
 <style scoped>
 .main-header{
-  width: 100%;
   color: #021B51;
   font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
   padding: 20px 40px;
 }
 .header-title{
   display: flex;
-  height: 40px;
   font-size: 1.6em;
 }
 
@@ -65,8 +83,54 @@ export default {
 }
 
 .subtitle {
-  margin: 15px 25px;
+  margin: 15px 0;
   font-size: 1.1em;
   color: #727D94;
+}
+
+.orders-table {
+  width: 1000px;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  margin-left: 15px;
+}
+
+thead {
+  color: #A1A8B8;
+}
+
+th {
+  min-width: 90px;
+}
+
+@media screen and (max-width: 450px){
+.header-title{
+font-size: 0.65em;
+}
+
+.header-title img {
+  width: 35px;
+  align-items: center;
+  height: auto;
+}
+
+.subtitle {
+  font-size: 0.5em;
+  margin: 7px 10px;
+}
+
+thead {
+  font-size: 0.6em;
+}
+
+.orders-table {
+  width: auto;
+}
+}
+
+
+@media screen and (min-width: 450px){
+.header-title{
+  height: 40px;
+}
 }
 </style>
